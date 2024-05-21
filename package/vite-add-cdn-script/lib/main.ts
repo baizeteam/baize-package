a9b5b6835fd39e7353daf0b280d4275b88513b50
xiaoyu <xiaoyu@sulg.top>
2024-05-21 23:25:04 +0800

    ✨ cdn添加异常处理

--- package/vite-add-cdn-script/lib/main.ts
+++ package/vite-add-cdn-script/lib/main.ts
@@ -64,19 +64,46 @@ function viteAddCdnScript(opt: IOptions): PluginOption {
         const packageJson = readFileSync(packageJsonPath, "utf-8");
         const packageData = JSON.parse(packageJson);
         const external = _config.build.rollupOptions.external;
-        let script = "";
+        const errorScript = `<script>
+  const separators = JSON.parse('${JSON.stringify(separators)}');
+  const cdnUrlObj = JSON.parse('${JSON.stringify(cdnUrlObj)}');
+  const defaultCdns = JSON.parse('${JSON.stringify(defaultCdns)}');
+  
+  function errorCDN(e) {
+    const nextCur = parseInt(e.getAttribute("data-cur")) + 1;
+    if(nextCur>=3){return;}
+    const filename = e.getAttribute("data-filename");
+    const key = e.getAttribute("data-key");
+    const urlName = defaultCdns[nextCur]
+
+    console.log(urlName,cdnUrlObj[urlName], key, separators[urlName], filename);
+    
+    // 组装新的cdn链接
+    const url = location.protocol + "//" + cdnUrlObj[urlName] + "/" + key + separators[urlName] + filename;
+    console.log(url);
+    const tagName = e.tagName
+    const cdnDOM = document.createElement(tagName);
+    cdnDOM.setAttribute(tagName === 'SCRIPT' ?'src' : 'href', url);
+    Object.keys(e.dataset).forEach(_key => {
+      cdnDOM.setAttribute('data-'+_key, e.dataset[_key]);
+    })
+    cdnDOM.setAttribute("data-cur", nextCur.toString());
+    cdnDOM.setAttribute("onerror", "errorCDN(this)");
+    document.head.appendChild(cdnDOM);
+    e.remove();
+  }
+</script>`;
+
+        let script = "" + errorScript;
         Object.keys(packageData.dependencies).forEach((key) => {
           if (external.includes(key)) {
             if (customScript[key]) {
               script += customScript[key];
             } else {
               const version = packageData.dependencies[key];
-              const fileName = npmProObj[key] ? npmProObj[key] : `${key}.min.js`;
-              const url = `${protocol}://${cdnUrlObj[urlName]}/${key}${separators[urlName]}${version.replace(
-                "^",
-                "",
-              )}/${fileName}`;
-              script += `<script src="${url}" type="text/javascript"></script>\n`;
+              const fileName = version.replace("^", "") + "/" + (npmProObj[key] ? npmProObj[key] : `${key}.min.js`);
+              const url = `${protocol}://${cdnUrlObj[urlName]}/${key}${separators[urlName]}${fileName}`;
+              script += `<script src="${url}" type="text/javascript" onerror="errorCDN(this)" data-cur="0" data-key="${key}" data-filename="${fileName}"></script>\n`;
             }
           }
         });
