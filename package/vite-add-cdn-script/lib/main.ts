301039ec710228b8bad385c3617ceded3abea35b
lgldlk <chenke@fenxianglife.com>
2024-06-04 18:53:29 +0800

    ✨增加api接入构建，测试

--- package/vite-add-cdn-script/lib/main.ts
+++ package/vite-add-cdn-script/lib/main.ts
@@ -1,39 +1,24 @@
 import path from "path";
 import fs from "fs";
 import { PluginOption } from "vite";
-import { getPackageVersion } from "./utils";
+import { getPackageURL, getPackageVersion } from "./utils";
+import { PropertyCdn } from "./types";
 
 enum EEnforce {
   PRE = "pre",
   POST = "post",
 }
 
-const cdnUrlObj = {
-  jsdelivr: "cdn.jsdelivr.net/npm",
-  unpkg: "unpkg.com",
-};
-
-const separators = {
-  jsdelivr: "@",
-  unpkg: "@",
-};
-
 export interface IOptions {
   protocol?: string;
   customScript?: { [key: string]: string };
   customFilepath?: { [key: string]: string };
   retryTimes?: number;
-  defaultCdns?: string[];
+  defaultCdns?: PropertyCdn[];
 }
 
 function viteAddCdnScript(opt: IOptions): PluginOption {
-  const {
-    protocol = "https",
-    customScript = {},
-    retryTimes = 1,
-    defaultCdns = ["jsdelivr", "unpkg"],
-    customFilepath = {},
-  } = opt;
+  const { protocol = "https", customScript = {}, retryTimes = 1, defaultCdns = ["jsdelivr", "unpkg"] } = opt;
   let _config;
   const _npmProObj = { ...customScript };
 
@@ -44,50 +29,73 @@ function viteAddCdnScript(opt: IOptions): PluginOption {
     config(confing) {
       _config = confing;
     },
-    transformIndexHtml(html) {
+    async transformIndexHtml(html) {
+      if (!defaultCdns || defaultCdns.length === 0) throw new Error("defaultCdns不能为空");
       const packageJsonPath = path.resolve(process.cwd(), "package.json");
       try {
-        const urlName = defaultCdns[0];
         const packageJson = fs.readFileSync(packageJsonPath, "utf-8");
         const packageData = JSON.parse(packageJson);
         const external = _config.build.rollupOptions.external;
-        const errorScript = `<script>
-  const separators = JSON.parse('${JSON.stringify(separators)}');
-  const cdnUrlObj = JSON.parse('${JSON.stringify(cdnUrlObj)}');
-  const defaultCdns = JSON.parse('${JSON.stringify(defaultCdns)}');
-  function errorCDN(e) {
-    const nextCur = parseInt(e.getAttribute("data-cur")) + 1;
-    if(nextCur>=${retryTimes}){return;}
-    const filename = e.getAttribute("data-filename");
-    const key = e.getAttribute("data-key");
-    const urlName = defaultCdns[nextCur]
-    // 组装新的cdn链接
-    const url = location.protocol + "//" + cdnUrlObj[urlName] + "/" + key + separators[urlName] + filename;
-    // 克隆原标签
-    const tagName = e.tagName
-    const cdnDOM = document.createElement(tagName);
-    cdnDOM.setAttribute(tagName === 'SCRIPT' ?'src' : 'href', url);
-    Object.keys(e.dataset).forEach(_key => {
-      cdnDOM.setAttribute('data-'+_key, e.dataset[_key]);
-    })
-    cdnDOM.setAttribute("data-cur", nextCur.toString());
-    cdnDOM.setAttribute("onerror", "errorCDN(this)");
-    document.head.appendChild(cdnDOM);
-    e.remove();
-  }
-</script>`;
-
-        let script = "" + errorScript;
-        external.forEach((key) => {
+        const packNameUrl: { [k in PropertyCdn]?: string[] } = {};
+        let script = "";
+        const urlListRes = await Promise.all(
+          external.map(async (key) => {
+            const version = getPackageVersion(packageData, key);
+            if (customScript[key]) {
+              return {
+                urls: [],
+                key,
+              };
+            }
+            if (!version) {
+              console.error(`package.json中不存在${key}的版本号`);
+              return;
+            }
+            return {
+              urls: await Promise.all(
+                defaultCdns.map(async (cdn) => {
+                  return await getPackageURL(key, version, cdn);
+                }),
+              ),
+              key,
+            };
+          }),
+        );
+        urlListRes.forEach((element) => {
+          if (!element) return;
+          const { urls, key } = element;
           if (customScript[key]) {
             script += customScript[key];
           } else {
-            const version = getPackageVersion(packageData, key);
-            const fileName = version + "/" + (_npmProObj[key] ? _npmProObj[key] : `dist/${key}.min.js`);
-            const url = `${protocol}://${cdnUrlObj[urlName]}/${key}${fileName}`;
-            script += `<script src="${url}" type="text/javascript" crossorigin="anonymous" onerror="errorCDN(this)" data-cur="0" data-key="${key}" data-filename="${fileName}"></script>\n`;
+            packNameUrl[key] = urls;
+            const url = urls[0];
+            script += `<script src="${url}" type="text/javascript" crossorigin="anonymous" onerror="errorCDN(this)" data-cur="0"  data-key="${key}"></script>\n`;
           }
         });
+        const errorScript = `<script>
+        function errorCDN(e) {
+          const packNameUrl = JSON.parse('${JSON.stringify(packNameUrl)}');
+          const nextCur = parseInt(e.getAttribute("data-cur")) + 1;
+          if(nextCur>=${retryTimes}){return;}
+          
+          const key = e.getAttribute("data-key");
+          if(nextCur>=packNameUrl[key].length){return;}
+          // 新的cdn链接
+          const url = packNameUrl[key][nextCur]
+          // 克隆原标签
+          const tagName = e.tagName
+          const cdnDOM = document.createElement(tagName);
+          cdnDOM.setAttribute(tagName === 'SCRIPT' ?'src' : 'href', url);
+          Object.keys(e.dataset).forEach(_key => {
+            cdnDOM.setAttribute('data-'+_key, e.dataset[_key]);
+          })
+          cdnDOM.setAttribute("data-cur", nextCur.toString());
+          cdnDOM.setAttribute("onerror", "errorCDN(this)");
+          document.head.appendChild(cdnDOM);
+          e.remove();
+        }
+      </script>`;
+        script = errorScript + script;
         html = html.replace("</head>", `${script}</head>`);
 
         return html;
--- package/vite-add-cdn-script/lib/utils.ts
+++ package/vite-add-cdn-script/lib/utils.ts
@@ -63,116 +63,214 @@ export const unpkgDirectoryHandle = (res: (unpkyDirectory | unpkgFiles)[]) => {
 /**
  * 各个类型cdn url file获取处理与统一
  */
-export const cdnUrlGet: {
-  [cdn in PropertyCdn]: (packageName: string, version: string) => Promise<FileNameRes>;
+export const cdnUrlGeterr: {
+  [cdn in PropertyCdn]: {
+    getFileList: (packageName: string, version: string) => Promise<FileNameRes>;
+    getUrl: (packageName: string, version: string, fileName: string) => string;
+  };
 } = {
-  jsdelivr: (packageName: string, version: string) => {
-    return new Promise<FileNameRes>((resolve, reject) => {
-      // /v1/stats/packages/npm/{package}@{version}/files
-      req.get(
-        `https://data.jsdelivr.com/v1/stats/packages/npm/${packageName}@${version}/files?period=day`,
-        (data: string) => {
-          const res: (FileNameRes["fileList"][number] & {
-            [x: string]: unknown;
-          })[] = JSON.parse(data);
-          if (res.length === 0) {
-            reject(new Error(`${packageName}@${version} not found`));
-            return;
-          }
-          resolve({ fileList: res });
-        },
-        (e: unknown) => {
-          reject(e);
-        },
-      );
-    });
-  },
-  unpkg: (packageName: string, version: string) => {
-    // unpkg.com/react@18.3.1/?meta
-    return new Promise<FileNameRes>((resolve, reject) => {
-      req.get(
-        `https://unpkg.com/${packageName}@${version}/?meta`,
-        (data: string) => {
-          const res: unpkgRes = JSON.parse(data);
-          resolve({ fileList: unpkgDirectoryHandle(res.files || []) });
-        },
-        (e: unknown) => {
-          reject(e);
-        },
-      );
-    });
-  },
-  bootcdn: (packageName: string, version: string) => {
-    return new Promise<FileNameRes>((resolve, reject) => {
-      // https://api.bootcdn.cn/libraries/react
-      req.get(
-        `https://api.bootcdn.cn/libraries/${packageName}`,
-        (data: string) => {
-          const res: bootcdnRes = JSON.parse(data);
-          if (res.length === 0) {
-            reject(new Error(`${packageName} not found in bootcdn`));
-            return;
-          }
-          // 一般第一项就是要找到的包，暂时没有遇到过在第二项的情况
-          const packageInfo = res[0];
-          const assets = packageInfo.assets;
-          const versionItem = assets.find((item) => {
-            return item.version === version;
-          });
-          if (!versionItem) {
-            reject(new Error(`${packageName}@${version} not found in ${packageInfo.name}`));
-            return;
-          }
-          // 加 / 与 上面两种cdn做统一
-          const fileList = versionItem.files.map((item) => {
-            return {
-              name: "/" + item,
-            };
-          });
+  jsdelivr: {
+    getFileList: (packageName: string, version: string) => {
+      return new Promise<FileNameRes>((resolve, reject) => {
+        // /v1/stats/packages/npm/{package}@{version}/files
+        req.get(
+          `https://data.jsdelivr.com/v1/stats/packages/npm/${packageName}@${version}/files`,
+          (data: string) => {
+            const res: (FileNameRes["fileList"][number] & {
+              [x: string]: unknown;
+            })[] = JSON.parse(data);
+            if (res.length === 0) {
+              reject(new Error(`${packageName}@${version} not found`));
+              return;
+            }
+            resolve({ fileList: res });
+          },
+          (e: unknown) => {
+            reject(e);
+          },
+        );
+      });
+    },
+    // 拼接url
 
-          resolve({ fileList, recommendFileName: packageInfo.filename });
-        },
-        (e: unknown) => {
-          reject(e);
-        },
-      );
-    });
+    getUrl: (packageName: string, version: string, fileName: string) => {
+      // https://cdn.jsdelivr.net/npm/package@version/file
+      return `https://cdn.jsdelivr.net/npm/${packageName}@${version}${fileName}`;
+    },
   },
-  cdnjs: (packageName: string, version: string) => {
-    return new Promise<FileNameRes>((resolve, reject) => {
-      // https://api.cdnjs.com/libraries/jquery/3.5.1
-      req.get(
-        `https://api.cdnjs.com/libraries/${packageName}/${version}`,
-        (data: string) => {
-          const res: cdnjsRes = JSON.parse(data);
-          if (res.error) {
-            reject(new Error(`cdnjs: ${packageName}@${version} not found`));
-            return;
-          }
-          resolve({
-            fileList: res.rawFiles.map((item) => {
+  unpkg: {
+    getFileList: (packageName: string, version: string) => {
+      // unpkg.com/react@18.3.1/?meta
+      return new Promise<FileNameRes>((resolve, reject) => {
+        req.get(
+          `https://unpkg.com/${packageName}@${version}/?meta`,
+          (data: string) => {
+            const res: unpkgRes = JSON.parse(data);
+            resolve({ fileList: unpkgDirectoryHandle(res.files || []) });
+          },
+          (e: unknown) => {
+            reject(e);
+          },
+        );
+      });
+    },
+    getUrl: (packageName: string, version: string, fileName: string) => {
+      // unpkg.com/:package@:version/:file
+      return `https://unpkg.com/${packageName}@${version}${fileName}`;
+    },
+  },
+  bootcdn: {
+    getFileList: (packageName: string, version: string) => {
+      return new Promise<FileNameRes>((resolve, reject) => {
+        // https://api.bootcdn.cn/libraries/react
+        req.get(
+          `https://api.bootcdn.cn/libraries/${packageName}`,
+          (data: string) => {
+            const res: bootcdnRes = JSON.parse(data);
+            if (res.length === 0) {
+              reject(new Error(`${packageName} not found in bootcdn`));
+              return;
+            }
+            // 一般第一项就是要找到的包，暂时没有遇到过在第二项的情况
+            const packageInfo = res[0];
+            const assets = packageInfo.assets;
+            const versionItem = assets.find((item) => {
+              return item.version === version;
+            });
+            if (!versionItem) {
+              reject(new Error(`${packageName}@${version} not found in ${packageInfo.name}`));
+              return;
+            }
+            // 加 / 与 上面两种cdn做统一
+            const fileList = versionItem.files.map((item) => {
               return {
                 name: "/" + item,
               };
-            }),
-          });
-        },
-        (e: unknown) => {
-          reject(e);
-        },
-      );
-    });
+            });
+
+            resolve({ fileList, recommendFileName: packageInfo.filename });
+          },
+          (e: unknown) => {
+            reject(e);
+          },
+        );
+      });
+    },
+    getUrl: (packageName: string, confirmVersion: string, fileName: string) => {
+      // https://cdn.bootcdn.net/ajax/libs/element-ui/2.15.14/index.min.js
+      return `https://cdn.bootcdn.net/ajax/libs/${packageName}/${confirmVersion}${fileName}`;
+    },
+  },
+  cdnjs: {
+    getFileList: (packageName: string, version: string) => {
+      return new Promise<FileNameRes>((resolve, reject) => {
+        // https://api.cdnjs.com/libraries/jquery/3.5.1
+        req.get(
+          `https://api.cdnjs.com/libraries/${packageName}/${version}`,
+          (data: string) => {
+            const res: cdnjsRes = JSON.parse(data);
+            if (res.error) {
+              reject(new Error(`cdnjs: ${packageName}@${version} not found`));
+              return;
+            }
+            resolve({
+              fileList: res.rawFiles.map((item) => {
+                return {
+                  name: "/" + item,
+                };
+              }),
+            });
+          },
+          (e: unknown) => {
+            reject(e);
+          },
+        );
+      });
+    },
+    getUrl: (packageName: string, version: string, fileName: string) => {
+      // https://cdnjs.cloudflare.com/ajax/libs/react-is/18.3.1/cjs/react-is.production.min.js
+      return `https://cdnjs.cloudflare.com/ajax/libs/${packageName}/${version}${fileName}`;
+    },
   },
 };
 
-const getPackageCdnUrl = ({
-  packageName,
-  version,
-  cdn,
-}: {
-  packageName: string;
-  version: string;
-  cdn: PropertyCdn;
-}) => {
-  return packageName.split("/").pop();
+/**
+ * 获取特定包在特定CDN上的URL。
+ *
+ * @param packageName 包名
+ * @param version 版本号
+ * @param cdn CDN类型
+ * @returns 返回特定包在特定CDN上的URL
+ *
+ * @throws 如果版本号不符合规定的格式，将抛出错误
+ */
+export const getPackageURL = async (packageName: string, version: string, cdn: PropertyCdn) => {
+  // 再这一步做分离是为了之后可能做 @ ~ 等符号的处理,🤔每个cdn的具体方案可能不同
+  const confirmVersion = version.match(/\d+(.\d+)?(.\d+)?/)?.[0];
+  if (!confirmVersion) {
+    throw new Error(`${packageName} version ${version} is not valid`);
+  }
+  const res = await cdnUrlGeterr[cdn].getFileList(packageName, confirmVersion).catch((e) => {
+    throw e;
+  });
+
+  const fileName = getPackageFile(res, packageName);
+  if (!fileName) {
+    throw new Error(
+      `Can't find the file of ${packageName}@${confirmVersion} in ${cdn}, please check the package name or version`,
+    );
+  }
+  return cdnUrlGeterr[cdn].getUrl(packageName, confirmVersion, fileName);
+};
+
+/**
+ * 获取推荐的文件名
+ *
+ * @param fileList 文件列表
+ * @param packageName 包名
+ * @returns 返回推荐的文件名 (如果有的话) 或空字符串 (没有匹配到)
+ */
+const getPackageFile = ({ fileList }: FileNameRes, packageName: string) => {
+  // 优先推荐文件
+  let recommendList: (RegExp | string)[] = [
+    `umd/${packageName}.production.min.js`,
+    /umd\/.+?\.production\.min\.js$/,
+    /dist\/.+?\.production\.min\.js$/,
+    /dist\/.+?\.umd\.min\.js$/,
+    `dist/${packageName}.prod.min.js`,
+    /dist\/.+?\.global.prod.min.js/,
+    `dist/${packageName}.min.js`,
+    /.+?\.global.prod.min.js/,
+    /.+?.global.prod.js/,
+    /lib\/.+?\.min\.js$/,
+    /dist\/.+?\.min\.js$/,
+    /index\.min\.js$/,
+    /index\.js$/,
+    /\.min\.js$/,
+    /\.js$/,
+  ];
+  // 黑名单匹配
+  const blackList = ["runtime", "compiler", ".esm", ".cjs", "development"].filter((item) => {
+    return !packageName.includes(item);
+  });
+
+  let selectFile = "";
+
+  for (let item of recommendList) {
+    if (item instanceof RegExp) {
+      selectFile =
+        fileList.find((file) => {
+          return item.test(file.name) && !blackList.some((blackItem) => file.name.includes(blackItem));
+        })?.name || "";
+    } else {
+      selectFile =
+        fileList.find((file) => {
+          return file.name.includes(item) && !blackList.some((blackItem) => file.name.includes(blackItem));
+        })?.name || "";
+    }
+    if (selectFile) {
+      break;
+    }
+  }
+  return selectFile;
 };
