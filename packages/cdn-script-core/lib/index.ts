ba314ca9907bb5c7f5f6b4c10b839a29f5c3b5ea
lgldlk <1851293758@qq.com>
2024-07-18 01:37:53 +0800

    feat: 封装业务逻辑也进入核心请求库

--- packages/cdn-script-core/lib/index.ts
+++ packages/cdn-script-core/lib/index.ts
@@ -1,4 +1,79 @@
+import { ConsoleManage } from "./consoleManage";
+import { findUrls, getPackageDependencies } from "./findUrls";
+
+import path from "path";
 export * from "./consoleManage";
 export * from "./generateScript";
 export * from "./findUrls";
 export * from "./ErrorTypes";
+import fs from "fs";
+import { PropertyCdn } from "./types";
+import { generateScript } from "./generateScript";
+
+export default async function getExternalScript({
+  libName,
+  customScript,
+  external,
+  defaultCdns,
+}: {
+  libName: string;
+  external: string[];
+  customScript: { [key: string]: string };
+  defaultCdns: PropertyCdn[];
+}) {
+  let consoleManage: ConsoleManage = new ConsoleManage(libName);
+  const packageJsonPath = path.resolve(process.cwd(), "package.json");
+  try {
+    const packageJson = fs.readFileSync(packageJsonPath, "utf-8");
+
+    const packageData = JSON.parse(packageJson);
+    const {
+      urls: urlListRes,
+      noVersionPackages,
+      errorList,
+    } = await findUrls({
+      external,
+      packageData,
+      customScript,
+      defaultCdns,
+    });
+    consoleManage.addMessageList("warn", errorList);
+    // 没有找到本地版本的库，在库中寻找对应的版本
+    if (noVersionPackages.length > 0) {
+      const { urlPackageJsonRes, errorList: packageDependErrorList } = await getPackageDependencies({
+        packageVersionInfo: urlListRes,
+      });
+      consoleManage.addMessageList("warn", packageDependErrorList);
+      const {
+        urls: noPackageUrls,
+        noVersionPackages: notFindPackages,
+        errorList,
+      } = await findUrls({
+        external: noVersionPackages,
+        packageData: urlPackageJsonRes,
+        customScript,
+        defaultCdns,
+      });
+      consoleManage.addMessageList("warn", errorList);
+      // 合并未找到版本的库的cdn地址列表（保持原有顺序）
+      noPackageUrls.map((item) => {
+        if (!item) return;
+        const { urls, key } = item;
+        urlListRes.find((item) => item?.key === key)?.urls.push(...urls);
+      });
+
+      if (notFindPackages.length > 0) {
+        console.error(`找不到${notFindPackages.join(",")}的版本`);
+        throw new Error(`找不到${notFindPackages.join(",")}的版本`);
+      }
+    }
+
+    consoleManage.consoleAll();
+
+    const script = generateScript(urlListRes);
+    return script;
+  } catch (error) {
+    consoleManage.consoleAll();
+    throw error;
+  }
+}
