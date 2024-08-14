bdf47b618dc3305be954ae8f473e3be36efe27e2
wengzehua <wengzehua@cvte.com>
2024-08-14 17:49:47 +0800

    docs: 更新index.d.ts

--- packages/baize-compress-image/index.d.ts
+++ packages/baize-compress-image/index.d.ts
@@ -1,2 +1,3 @@
 export function compressImageWorker(file: File, quality?: number): Promise<File>;
-export function compressImagesWorker(files: File[], quality?: number): Promise<File[]>;
+export function compressImagesWorker(files: File[], quality?: number): Promise<PromiseSettledResult<File>[]>;
+export function cancelAllCompressWorker(): void;
